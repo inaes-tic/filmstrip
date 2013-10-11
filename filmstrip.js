8c15545a39a2b22267ddaa3f4355dd93a32b827b
Niv Sardi <xaiki@evilgiggle.com>
2013-10-11 14:31:01 -0300

    add licence header to js files
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- filmstrip.js
+++ filmstrip.js
@@ -1,3 +1,23 @@
+/*
+ * Filmstrip.js: an HTML5 video preview widget that will rock your world
+ *   Copyright © <2013> Instituto Nacional de Associativismo y Economia Social.
+ *   Copyright © <2013> Cooperativa de Trabajo OpCode Limitada <info@opcode.coop>.
+ *   Copyright © <2013> Leonardo Vidarte <leo@opcode.coop>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 var Filmstrip = function(model, args) {
 
     this.model = null;
